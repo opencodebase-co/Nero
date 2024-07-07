fbf4988f28a21926ec91405d324c1b0e1c51070a
v182 <142740981+v182@users.noreply.github.com>
2024-07-07 15:09:37 +0200

    security issue fixed

--- assets/js/google-charts.js
+++ assets/js/google-charts.js
@@ -4,7 +4,7 @@ google.charts.load('current', {
   'packages': ['geochart'],
   // Note: you will need to get a mapsApiKey for your project.
   // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
-  'mapsApiKey': 'AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY'
+  'mapsApiKey': 'mapsApiKey'
 });
 google.charts.setOnLoadCallback(drawRegionsMap);
 
